96b62c4d2028a2bdcf1cc5ddf05f404ffc14e76a
georgeUrsachi <george.ursachi@lenus.io>
2022-09-25 23:14:52 +0200

    Added sidebar Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React, { FC } from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import Navigation from "./components/Navigation";
+import { FC } from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import Navigation from './components/Navigation'
 const darkTheme = createTheme({
   palette: {
-    mode: "dark",
+    mode: 'dark',
   },
-});
+})
 
-const App = () => {
+const App: FC<{}> = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Navigation />
-    </ThemeProvider>
-  );
-};
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Navigation />
+      </ThemeProvider>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,82 @@
-import React from "react";
-import { Fab } from "@mui/material";
-import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
+import { FC, useState, MouseEvent, KeyboardEvent, ReactNode } from 'react'
+import {
+  Fab,
+  Drawer,
+  List,
+  ListItem,
+  Box,
+  ListItemIcon,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material'
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow'
+import ColorLensIcon from '@mui/icons-material/ColorLens'
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+import LoginIcon from '@mui/icons-material/Login'
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration'
 
-const Navigation = () => {
+const Navigation: FC<{}> = () => {
+  const anchor = 'left'
+
+  const menuSections: { name: string; icon: ReactNode }[] = [
+    {
+      name: 'Artwork Gallery',
+      icon: <ColorLensIcon />,
+    },
+    { name: 'Artist Roster', icon: <AccountBoxIcon /> },
+    { name: 'Log In', icon: <LoginIcon /> },
+    { name: 'Register', icon: <AppRegistrationIcon /> },
+  ]
+
+  const [open, setOpen] = useState(false)
+  const toggleDrawer =
+    (open: boolean) => (event: MouseEvent | KeyboardEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as KeyboardEvent).key === 'Tab' ||
+          (event as KeyboardEvent).key === 'Shift')
+      ) {
+        return
+      }
+
+      setOpen(open)
+    }
+
   return (
-    <Fab color="primary">
-      <DoubleArrowIcon />
-    </Fab>
-  );
-};
+    <>
+      <Fab
+        color="primary"
+        size="medium"
+        onClick={toggleDrawer(true)}
+        sx={{
+          position: 'fixed',
+          top: '50%',
+          left: 10,
+        }}
+      >
+        <DoubleArrowIcon />
+      </Fab>
+      <Drawer anchor={anchor} open={open} onClose={toggleDrawer(false)}>
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            {menuSections.map((section) => (
+              <ListItem key={section.name} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{section.icon}</ListItemIcon>
+                  <ListItemText primary={section.name} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  )
+}
 
-export default Navigation;
+export default Navigation
