18585ba36ce8af1e546df39a5065267bb354bccb
georgeUrsachi <george.ursachi@lenus.io>
2022-09-25 16:55:26 +0200

    New and updated project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +18,6 @@ const App = () => {
       </header>
     </div>
   );
-};
+}
 
 export default App;
